965fc3307e3c55315260281ab6a625867b84f29d
Jacob Montpetit <jacobmontpetit@Jacobs-MacBook-Pro.local>
2017-07-20 23:32:54 -0400

    update user needs a lot of work

--- Server/src/clojure_rest_api/handler.clj
+++ Server/src/clojure_rest_api/handler.clj
@@ -4,17 +4,17 @@
             [compojure.route :as route]
             [ring.middleware.json :as middleware]
             [ring.util.response :refer [response]]
-
             [clojure-rest-api.dbmanager :as dbman]))
 
 
 (defroutes app-routes 
   (context "/User" [] (defroutes User-routes
     (GET "/" [] (response (dbman/get-user)))
-    (GET "/:id" [:id] (response dbman/get-user :User id) )
-    (POST "/" [] (response {:eat "poop"}) )
+    (GET "/:UserName" [UserName] (response (dbman/get-user :User UserName) ))
+    (POST "/" {body :body}  (dbman/insert-user body ))
+    (PUT  "/" {body :body header :header-params} (response  {:body body :headers header}) ))) ;(dbman/update-user ((:User body ) (hash-map (:Update body)) )))
                        
-                       ))
+                       
   (GET "/" [] "HELOO BABY BOY")
   (route/not-found "Not Found try again"))
 
