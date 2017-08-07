9ae8e171d8815df9bb37c4d79f2c135aeed9e5c0
Muhammed Khalid <mkhalid@Uzairs-MacBook-Air.local>
2017-08-07 19:55:55 -0400

    successful build

--- Chatter/app/src/main/java/com/example/mkhalid/chatter/HomeActivity.java
+++ Chatter/app/src/main/java/com/example/mkhalid/chatter/HomeActivity.java
@@ -18,7 +18,8 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("HELLO BABY BOY");
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
